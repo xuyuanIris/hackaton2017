97cc8de10ca53093fa56a81f8d9c4d1308b5d994
xiongjie <xiongj01@mysoft.com.cn>
2017-06-23 18:04:35 +0800

    修改eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,12 @@ module.exports = {
     "parser": "babel-eslint",
     "extends": "airbnb",
     "rules": {
-        "react/jsx-filename-extension": 0
+        "react/jsx-filename-extension": 0,
+        "comma-dangle": 0,
+        "semi": 0,
+        'indent': [2, 4, {"SwitchCase": 1, "VariableDeclarator": 1}],
+        'react/jsx-indent': 0,
+        "eol-last": 0
     },
     "globals": {
         "window": false

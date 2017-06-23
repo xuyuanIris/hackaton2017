275205b73b625e8e94391c4c15a9a9c4ebf868ad
xiongjie <xiongj01@mysoft.com.cn>
2017-06-23 18:05:07 +0800

    修改启动项 (react-router redux)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createHistory from 'history/createBrowserHistory';
+import { Route } from 'react-router';
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, window.document.getElementById('root'));
+const history = createHistory();
+
+const middleware = routerMiddleware(history);
+
+const store = createStore(
+    combineReducers({
+        ...reducers,
+        router: routerReducer
+    }),
+    applyMiddleware(middleware)
+)
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <div>
+                123
+                <Route exact path="/*" component={App} />
+                <Route exact path="/1" component={App} />
+            </div>
+        </ConnectedRouter>
+    </Provider>,
+    window.document.getElementById('root')
+)
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { handleActions } from 'redux-actions'
+
+const reducer = handleActions({
+    xxx: () => { }
+}, {})
+
+export default reducer;

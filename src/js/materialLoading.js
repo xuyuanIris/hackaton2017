66eba288e127e2f34b1ac8665aa34c7eb2862138
cexoso <285516193@qq.com>
2017-06-24 13:16:42 +0800

    loading style

---
+++ src/js/materialLoading.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import style from 'styled-components'
+import CircularProgress from 'material-ui/CircularProgress';
+import Paper from 'material-ui/Paper';
+
+const CenterPaper = style(Paper) `
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    padding: 20px 70px;
+    border-radius: 10px;
+    transform: translate(-50%,-50%);
+`
+export default () => (<CenterPaper>
+    <CircularProgress />
+</CenterPaper>)

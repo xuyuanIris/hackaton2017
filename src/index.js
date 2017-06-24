ee20503c63e7801be18938813e4ce08abb85df98
cexoso <285516193@qq.com>
2017-06-24 13:17:48 +0800

    Perfect asycn Router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createHistory from 'history/createBrowserHistory';
 import { Route } from 'react-router';
 import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
-import App from './App';
+import './polyfill'
+import asyncMall from './mall/pages/async'
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import reducers from './reducers';
 
 const history = createHistory();
-
 const middleware = routerMiddleware(history);
 
 const store = createStore(
@@ -24,11 +25,13 @@ const store = createStore(
 ReactDOM.render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
-            <div>
-                123
-                <Route exact path="/*" component={App} />
-                <Route exact path="/1" component={App} />
-            </div>
+            <MuiThemeProvider>
+                <Route
+                    exact
+                    path="/"
+                    component={asyncMall}
+                />
+            </MuiThemeProvider>
         </ConnectedRouter>
     </Provider>,
     window.document.getElementById('root')

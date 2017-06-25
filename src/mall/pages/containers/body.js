0e045433e3f0d30adee315e03e873d91c5c6ed87
cexoso <285516193@qq.com>
2017-06-25 10:51:47 +0800

    should use body

---
+++ src/mall/pages/containers/body.js
@@ -0,0 +1,12 @@
+import style from 'styled-components';
+
+const body = style.div`
+    position: absolute;
+    width: 100%;
+    bottom: 60px;
+    top: 0;
+    left: 0;
+    overflow: auto;
+    overflow-scrolling: touch;
+`
+export default body
--- src/mall/pages/index.js
+++ src/mall/pages/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { Route, Redirect, Switch } from 'react-router'
 import MallContainer from './containers/mallContainer'
 import Footer from './containers/footer'
+import Body from './containers/body'
 import orders from './orders/index'
 import me from './me/index'
 
 export default () => (
     <MallContainer>
-        <Switch>
-            <Route path="/mall/orders" component={orders} />
-            <Route path="/mall/me" component={me} />
-            <Redirect to="/mall/orders" />
-        </Switch>
+        <Body>
+            <Switch>
+                <Route path="/mall/orders" component={orders} />
+                <Route path="/mall/me" component={me} />
+                <Redirect to="/mall/orders" />
+            </Switch>
+        </Body>
         <Footer />
     </MallContainer>
 )

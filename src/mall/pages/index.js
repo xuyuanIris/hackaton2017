392fc05e869ec93fd2dbdc93a2ded34b8cc558b6
cexoso <285516193@qq.com>
2017-06-25 09:18:43 +0800

    switch

--- src/mall/pages/index.js
+++ src/mall/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router'
+import { Route, Redirect, Switch } from 'react-router'
 import MallContainer from './containers/mallContainer'
 import Footer from './containers/footer'
 import orders from './orders/index'
@@ -7,8 +7,11 @@ import me from './me/index'
 
 export default () => (
     <MallContainer>
-        <Route path="/mall/orders" component={orders} />
-        <Route path="/mall/me" component={me} />
+        <Switch>
+            <Route path="/mall/orders" component={orders} />
+            <Route path="/mall/me" component={me} />
+            <Redirect to="/mall/orders" />
+        </Switch>
         <Footer />
     </MallContainer>
 )

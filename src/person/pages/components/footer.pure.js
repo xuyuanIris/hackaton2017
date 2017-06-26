3549dc2415115eaa9fee8a1f67f4534870c3d2fe
xiongjie <xiongj01@mysoft.com.cn>
2017-06-26 11:24:43 +0800

    footer解耦

---
+++ src/person/pages/components/footer.pure.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Paper from 'material-ui/Paper';
+import { BottomNavigation, BottomNavigationItem } from 'material-ui/BottomNavigation';
+import Setting from 'material-ui/svg-icons/action/settings-applications';
+import ActionHome from 'material-ui/svg-icons/action/home';
+import style from 'styled-components';
+import { compose, defaultProps, onlyUpdateForKeys } from 'recompose'
+
+const me = <Setting />
+const home = <ActionHome />
+const BottomPaper = style(Paper) `
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+`
+const Footer = ({ selectedIndex, replace }) => (<BottomPaper zDepth={1}>
+    <BottomNavigation selectedIndex={selectedIndex}>
+        <BottomNavigationItem label="商城大厅" icon={home} onTouchTap={replace('/person/malls')} />
+        <BottomNavigationItem label="我" icon={me} onTouchTap={replace('/person/me')} />
+    </BottomNavigation>
+</BottomPaper>)
+
+export default compose(
+    onlyUpdateForKeys(['selectedIndex']),
+    defaultProps({ selectedIndex: 0, replace: () => () => { } })
+)(Footer)

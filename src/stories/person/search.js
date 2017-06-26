a09fbe395a81da770f65d11dbff3542ddda198af
徐媛 <xuy04@mysoft.com.cn>
2017-06-26 16:57:04 +0800

    add input search component

--- src/stories/person/search.js
+++ src/stories/person/search.js
@@ -3,11 +3,10 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import Footer from './../../person/pages/components/footer.pure'
+import Search from './../../person/pages/malls/search'
 
-storiesOf('person', module).add('footer', () => (<MuiThemeProvider>
-    <Footer selectedIndex={0} replace={action} />
+storiesOf('person', module).add('search', () => (<MuiThemeProvider>
+    <Search />
 </MuiThemeProvider>))

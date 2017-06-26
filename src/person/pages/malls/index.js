6388f190aaf35488964f36b5119ef7266509c026
xiongjie <xiongj01@mysoft.com.cn>
2017-06-26 10:26:22 +0800

    添加mall结构，为malls添加逻辑结构代码

--- src/person/pages/malls/index.js
+++ src/person/pages/malls/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { compose } from 'recompose'
 import Top from './../../../js/top'
 
-export default () => (<div>
+const malls = () => (<div>
     <Top title="商城" />
     123123
 </div>)
+
+export default compose(
+
+)(malls)

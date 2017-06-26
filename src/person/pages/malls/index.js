1630cad838b5349f649c0ed0a030138d8b48de12
cexoso <285516193@qq.com>
2017-06-26 22:29:51 +0800

    实际图card展示

--- src/person/pages/malls/index.js
+++ src/person/pages/malls/index.js
@@ -2,11 +2,18 @@ import React from 'react'
 import { compose } from 'recompose'
 import Top from './../../../js/top'
 import Search from './search'
+import Card from './card'
+import List from './../containers/list'
 
 const malls = () => (<div>
     <Top title="商城" />
     <Search />
-    123123
+    <List>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+    </List>
 </div>)
 
 export default compose(

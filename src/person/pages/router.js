a8182e8a0e0e5d0ce7a922993206a5e812d1d92b
xiongjie <xiongj01@mysoft.com.cn>
2017-06-26 11:24:27 +0800

    添加me 路由

--- src/person/pages/router.js
+++ src/person/pages/router.js
@@ -4,8 +4,10 @@ import { nest } from 'recompose'
 import fadeIn from './../../js/fadeIn'
 import malls from './malls/index'
 import container from './container'
+import me from './me'
 
 export default () => (<Switch>
     <Route path="/person/malls" component={nest(container, fadeIn, malls)} />
+    <Route path="/person/me" component={nest(container, fadeIn, me)} />
     <Redirect to="/person/malls" />
 </Switch>)

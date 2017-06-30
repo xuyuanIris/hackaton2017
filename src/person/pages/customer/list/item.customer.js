7c180c868e7fe6534f7bd3733f35c4545fabbd8c
徐媛 <xuy04@mysoft.com.cn>
2017-06-30 11:36:25 +0800

    客户列表优化

--- src/person/pages/customer/list/item.customer.js
+++ src/person/pages/customer/list/item.customer.js
@@ -24,7 +24,8 @@ export default () => (<ItemWrap>
     <div style={{
         flex: 1,
         textAlign: 'center'
-    }}>
+    }}
+    >
         <Name>
             姓名：xiaomin
         </Name>
--- src/person/pages/customer/list/list.js
+++ src/person/pages/customer/list/list.js
@@ -2,9 +2,29 @@ import React from 'react'
 import { times } from 'lodash'
 import Item from './item.customer'
 import Top from './../../../../js/top'
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
+const add = {
+    color: '#fff',
+    height: 52,
+    padding: '0 10px'
+}
 const list = () => (<div>
-    <Top title="客户列表" />
+    <Top
+    title="客户列表"
+    iconElementRight={
+        <ContentAdd style={add} />
+        }
+    />
+    <div style={{
+        padding: '10px 15px',
+        lineHeight: '25px',
+        borderBottom: '1px solid #ccc'
+
+    }}
+    >
+        深圳市铂丽装饰设计工程有限公司
+    </div>
     {times(10, (key) => <Item key={key} />)}
 </div>)
 export default list;

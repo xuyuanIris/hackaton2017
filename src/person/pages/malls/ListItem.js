2c88314edf62431baec916c9e1a95ac6cc37581d
xiongjie <xiongj01@mysoft.com.cn>
2017-07-02 01:10:34 +0800

    eslint

--- src/person/pages/malls/ListItem.js
+++ src/person/pages/malls/ListItem.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { ListItem } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 import Avatar from 'material-ui/Avatar';
-import { grey500, redA700,blue700 } from 'material-ui/styles/colors';
+import { grey500, redA700, blue700 } from 'material-ui/styles/colors';
 
 const ListExampleMessages = (props) => {
-    const { src, name, remark, tel, amount,commission, onClick } = props
+    const { src, name, remark, amount, commission, onClick } = props
     return (<div onClick={onClick}>
         <ListItem
             leftAvatar={<Avatar src={src} />}
@@ -16,13 +16,13 @@ const ListExampleMessages = (props) => {
                         {remark}
                     </span>
                     <br />
-                    <span style={{ color: redA700, fontSize:18,marginRight:2 }}>{amount}</span>
-    <i style={{ color: blue700, fontSize:13,marginLeft:2 }}>{commission}</i>
+                    <span style={{ color: redA700, fontSize: 18, marginRight: 2 }}>{amount}</span>
+                    <i style={{ color: blue700, fontSize: 13, marginLeft: 2 }}>{commission}</i>
                 </p>
             }
             secondaryTextLines={2}
         />
-        <Divider inset/>
+        <Divider inset />
     </div>)
 };
 

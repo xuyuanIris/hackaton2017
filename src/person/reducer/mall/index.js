133b4c9d8d999911527400af92225ebe80ee197a
xiongjie <xiongj01@mysoft.com.cn>
2017-07-01 22:21:24 +0800

    more fp

--- src/person/reducer/mall/index.js
+++ src/person/reducer/mall/index.js
@@ -7,12 +7,10 @@ export const { getList, onChange } = createActions({
     getList: identity,
     onChange: identity
 })
-export const getCompanyList = () => (dispatch) => {
-    proxyAxios.get('/api/company/list')
-        .then(property('data.data'))
-        .then(getList)
-        .then(dispatch)
-}
+export const getCompanyList = () => dispatch => proxyAxios.get('/api/company/list')
+    .then(property('data.data'))
+    .then(getList)
+    .then(dispatch)
 export default handleActions(
     {
         [getList]: (state, actions) => immutable.set(state, 'list', actions.payload),

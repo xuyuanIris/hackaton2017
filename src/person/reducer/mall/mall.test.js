b47b50994d07c0ab74749bc94333dbf8fde788e0
xiongjie <xiongj01@mysoft.com.cn>
2017-07-01 16:55:14 +0800

    add spec

--- src/person/reducer/mall/mall.test.js
+++ src/person/reducer/mall/mall.test.js
@@ -1,9 +1,16 @@
 import { expect } from 'chai'
-import reducer, { getList } from './index'
+import reducer, { getList, onChange } from './index'
 
 describe('request', () => {
     it('doRequest', () => {
         const state = reducer({ list: [] }, getList([1, 2, 3]))
         expect(state.list).has.length(3)
+    });
+});
+
+describe('searchKey', () => {
+    it('onChange', () => {
+        const state = reducer({ searchKey: '' }, onChange('onChange'))
+        expect(state.searchKey).has.eq('onChange')
     });
 });

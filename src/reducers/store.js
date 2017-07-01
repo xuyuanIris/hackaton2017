aa9b8099189cef4afe55522caef3de18144ff961
xiongjie <xiongj01@mysoft.com.cn>
2017-07-01 12:00:59 +0800

    aop axios

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,14 +1,24 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createHistory from 'history/createBrowserHistory';
 import { routerMiddleware } from 'react-router-redux';
+import thunk from 'redux-thunk';
 import reducer, { injectReducer } from './reducer'
+import ProxyAxios from './../js/proxyAxios'
 
 export const history = createHistory();
 const middleware = routerMiddleware(history);
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducer,
-    composeEnhancers(applyMiddleware(middleware))
+    composeEnhancers(applyMiddleware(thunk, middleware))
 )
+export const proxyAxios = new ProxyAxios({
+    before(...o) {
+        console.log(...o)
+    },
+    after(...o) {
+        console.log(...o)
+    }
+})
 export const injectReducerWithStore = injectReducer(store)
 export default store

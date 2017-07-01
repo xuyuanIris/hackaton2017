cacd11f7567d45acff32616faf508b3f0eafe2c4
xiongjie <xiongj01@mysoft.com.cn>
2017-07-02 00:52:14 +0800

    eslint

--- src/person/pages/customer/new/newForm.js
+++ src/person/pages/customer/new/newForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { partial, property, map, includes } from 'lodash'
-import { compose, lifecycle, withHandlers, withState } from 'recompose'
+import { partial, property } from 'lodash'
+import { compose, lifecycle, withHandlers } from 'recompose'
 import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import Divider from 'material-ui/Divider';
@@ -29,70 +29,83 @@ const raisedButton = {
     flex: 1,
     margin: '50px 12px'
 }
-const NewForm = ({ setValue: _setValue }) => (<div>
-    <TextField
-        name="name"
-        hintText="请输入姓名"
-        floatingLabelText="姓名"
-        onChange={(e) => _setValue('name', e.target.value)}
-        fullWidth
-        floatingLabelStyle={textStyle}
-        inputStyle={textStyle}
-        hintStyle={textStyle}
-    />
-    <div style={{
-        display: 'flex',
-        padding: '25px 10px 5px'
-    }}
-    >
-        <span style={{ flex: 1, color: 'rgba(0, 0, 0, 0.3)' }} >
-            性别
+const NewForm = ({
+    name, gender,
+    mobile_tel: mobileTel,
+    remark,
+    setValue: _setValue,
+    reset: _reset
+}) => (<div>
+        <TextField
+            name="name"
+            hintText="请输入姓名"
+            floatingLabelText="姓名"
+            value={name}
+            onChange={(e) => _setValue('name', e.target.value)}
+            fullWidth
+            floatingLabelStyle={textStyle}
+            inputStyle={textStyle}
+            hintStyle={textStyle}
+        />
+        <div style={{
+            display: 'flex',
+            padding: '25px 10px 5px'
+        }}
+        >
+            <span style={{ flex: 1, color: 'rgba(0, 0, 0, 0.3)' }} >
+                性别
         </span>
-        <RadioButtonGroup name="gender" onChange={(e, v) => _setValue('gender', v)}>
-            <RadioButton
-                value={0}
-                label="男"
-                style={styles.radioButton}
-            />
-            <RadioButton
-                value={1}
-                label="女"
-                style={styles.radioButton}
-            />
-        </RadioButtonGroup>
-    </div>
-    <Divider />
-    <TextField
-        name="tel"
-        hintText="请输入电话号码"
-        floatingLabelText="电话号码"
-        onChange={(e) => _setValue('mobile_tel', e.target.value)}
-        fullWidth
-        floatingLabelStyle={textStyle}
-        inputStyle={textStyle}
-        hintStyle={textStyle}
-    />
-    <TextField
-        name="remark"
-        hintText="请输入客户装修偏好和其他基本情况"
-        floatingLabelText="描述说明"
-        onChange={(e) => _setValue('remark', e.target.value)}
-        fullWidth
-        multiLine
-        rowsMax={5}
-        floatingLabelStyle={textStyle}
-        inputStyle={textStyle}
-        hintStyle={textStyle}
-    />
-    <div style={{
-        display: 'flex',
-        textAlign: 'center'
-    }}
-    >
-        <RaisedButton label="重置" style={raisedButton} />
-        <RaisedButton label="确定" primary style={raisedButton} />
-    </div>
-</div>)
+            <RadioButtonGroup
+                name="gender"
+                valueSelected={gender}
+                onChange={(e, v) => _setValue('gender', v)}
+            >
+                <RadioButton
+                    value={0}
+                    label="男"
+                    style={styles.radioButton}
+                />
+                <RadioButton
+                    value={1}
+                    label="女"
+                    style={styles.radioButton}
+                />
+            </RadioButtonGroup>
+        </div>
+        <Divider />
+        <TextField
+            name="tel"
+            hintText="请输入电话号码"
+            value={mobileTel}
+            floatingLabelText="电话号码"
+            onChange={(e) => _setValue('mobile_tel', e.target.value)}
+            fullWidth
+            floatingLabelStyle={textStyle}
+            inputStyle={textStyle}
+            hintStyle={textStyle}
+        />
+        <TextField
+            name="remark"
+            hintText="请输入客户装修偏好和其他基本情况"
+            floatingLabelText="描述说明"
+            value={remark}
+            onChange={(e) => _setValue('remark', e.target.value)}
+            fullWidth
+            multiLine
+            rowsMax={5}
+            floatingLabelStyle={textStyle}
+            inputStyle={textStyle}
+            hintStyle={textStyle}
+        />
+        <div style={{
+            display: 'flex',
+            textAlign: 'center'
+        }}
+        >
+            <RaisedButton label="重置" style={raisedButton} onClick={_reset} />
+            <RaisedButton label="确定" primary style={raisedButton} />
+        </div>
+    </div>)
 
 export default compose(
     connect(

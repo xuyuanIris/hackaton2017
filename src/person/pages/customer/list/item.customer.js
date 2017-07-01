d8ee78b64ec1ef9e505a678f4710bfcc72a64a2b
xiongjie <xiongj01@mysoft.com.cn>
2017-07-01 19:28:54 +0800

    ui

--- src/person/pages/customer/list/item.customer.js
+++ src/person/pages/customer/list/item.customer.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import style from 'styled-components'
-import RaisedButton from 'material-ui/RaisedButton'
+import Checkbox from 'material-ui/Checkbox';
 
 const ItemWrap = style.div`
     display: flex;
     align-items: center;
+    
     border-bottom: 1px solid rgba(238, 238, 238, 0.8);
     padding: 10px 20px;
     line-height: 25px;
-    font-size: 14px;    
+    font-size: 14px;
+    *zoom: 1;
+    &:after {
+        clear: both;
+        height: 0;
+        display: block;
+        visibility: hidden;
+        content: "";
+    }
 `
 const Tel = style.div`
     font-size: 12px;
@@ -17,21 +26,22 @@ const Tel = style.div`
 const Name = style.div`
     text-align: left;
 `
-const raisedButton = {
-    height: 36
+const checkbox = {
 }
+const Label = style.div`
+`
 export default () => (<ItemWrap>
-    <div style={{
-        flex: 1,
-        textAlign: 'center'
-    }}
-    >
-        <Name>
-            姓名：xiaomin
-        </Name>
-        <Tel>
-            电话：13377829475
-        </Tel>
-    </div>
-    <RaisedButton label="推荐" primary={true} style={raisedButton} />
+    <Checkbox
+        style={checkbox}
+        label={
+            <Label>
+                <Name>
+                    姓名：xiaomin
+                </Name>
+                <Tel>
+                    电话：13377829475
+                </Tel>
+            </Label>
+        }
+    />
 </ItemWrap>)

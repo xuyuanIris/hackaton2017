f09d86fd9eae4f0d12b117457613b8dd2ae632bd
xiongjie <xiongj01@mysoft.com.cn>
2017-07-01 20:30:51 +0800

    bind actions and data

--- src/person/pages/customer/list/list.js
+++ src/person/pages/customer/list/list.js
@@ -1,29 +1,77 @@
 import React from 'react'
-import { times } from 'lodash'
+import { partial, property, map } from 'lodash'
+import { compose, lifecycle } from 'recompose'
+import { createSelector } from 'reselect'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import Checkbox from 'material-ui/Checkbox';
+import RaisedButton from 'material-ui/RaisedButton';
+import style from 'styled-components'
 import Item from './item.customer'
 import Top from './../../../../js/top'
-import ContentAdd from 'material-ui/svg-icons/content/add';
+import { getCustomerList } from '../../../reducer/customer/list/index'
 
+const raisedButton = {
+    flex: 1
+}
 const add = {
     color: '#fff',
     padding: '0 10px'
 }
-const list = () => (<div>
-    <Top
-    title="客户列表"
-    iconElementRight={
-        <ContentAdd style={add} />
-        }
-    />
-    <div style={{
-        padding: '10px 15px',
-        lineHeight: '25px',
-        borderBottom: '1px solid rgba(238, 238, 238, 0.8)'
+const Body = style.div`
+`
+const list = ({ cutList }) => {
+    return (<div>
+        <Top
+            title="客户列表"
+            iconElementRight={
+                <ContentAdd style={add} />
+            }
+        />
+        <div style={{
+            padding: '10px 15px',
+            lineHeight: '25px',
+            borderBottom: '1px solid rgba(238, 238, 238, 0.8)'
 
-    }}
-    >
-        深圳市铂丽装饰设计工程有限公司
+        }}
+        >
+            深圳市铂丽装饰设计工程有限公司
     </div>
-    {times(10, (key) => <Item key={key} />)}
-</div>)
-export default list;
+        <Body>
+            {
+                map(cutList, ({ id, mobile_tel: tel, remark, gender, name }) => (<Item
+                    tel={tel}
+                    key={id}
+                    name={name}
+                    remark={remark}
+                    gender={gender}
+                />))
+            }
+        </Body>
+        <div style={{
+            display: 'flex',
+            textAlign: 'center'
+        }}
+        >
+            <RaisedButton label="推荐" primary style={raisedButton} />
+        </div>
+    </div>)
+}
+
+export default compose(
+    connect(
+        createSelector(
+            [property('person.customer.list.list')],
+            (cutList) => ({
+                cutList
+            })
+        ),
+        partial(bindActionCreators, { getCustomerList })
+    ),
+    lifecycle({
+        componentDidMount() {
+            this.props.getCustomerList()
+        }
+    })
+)(list)

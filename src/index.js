9d74b5213801e57e6f04ef01de6a5350ee9bf2b8
xiongjie <xiongj01@mysoft.com.cn>
2017-07-01 10:39:44 +0800

    配置样式

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { Provider } from 'react-redux';
 import { Route } from 'react-router';
 import { ConnectedRouter } from 'react-router-redux';
@@ -10,18 +11,20 @@ import AsyncPerson from './person/pages/async'
 import registerServiceWorker from './registerServiceWorker';
 import store, { history } from './reducers/store'
 import './index.css';
+import theme from './theme.config'
 
+const muitheme = getMuiTheme(theme)
 ReactDOM.render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
             <div>
-                <MuiThemeProvider>
+                <MuiThemeProvider muiTheme={muitheme}>
                     <Route
                         path="/mall"
                         component={AsyncMall}
                     />
                 </MuiThemeProvider>
-                <MuiThemeProvider>
+                <MuiThemeProvider muiTheme={muitheme}>
                     <Route
                         path="/person"
                         component={AsyncPerson}

80d58a6af34b80713750f70881cd8a08dc6cdd4b
xiongjie <xiongj01@mysoft.com.cn>
2017-07-02 09:36:40 +0800

    自定制

--- src/mall/pages/orders/search.js
+++ src/mall/pages/orders/search.js
@@ -1,15 +1,19 @@
-import { compose, withHandlers } from 'recompose'
+import { compose, withHandlers, withProps } from 'recompose'
+import TextField from 'material-ui/TextField';
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { partial, constant } from 'lodash'
-import search from './../../../person/pages/components/search.pure';
 
 const emhance = compose(
     connect(
         constant({}),
-        partial(bindActionCreators, { })
+        partial(bindActionCreators, {})
     ),
     withHandlers({
+    }),
+    withProps({
+        fullWidth: true,
+        hintText: '输入电话号码或姓名进行查询'
     })
 )
-export default emhance(search)
+export default emhance(TextField)
